420f424eb32b288d9ccacae06bf6752c60c9ec17
shaileshkumar <shaileshsai457@gmail.com>
2025-05-02 16:57:28 +0530

    my commit

--- src/main/java/com/luv2code/springboot/cruddemorestapi/DemoController.java
+++ src/main/java/com/luv2code/springboot/cruddemorestapi/DemoController.java
@@ -9,16 +9,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+
 @Controller
 public class DemoController {
 
 
 
-    @GetMapping("/")
-    public String sayHello(Model model){
-        LocalDateTime now = LocalDateTime.now();
-        String formattedDate = now.format(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss"));
-        model.addAttribute("currentDate", formattedDate);
+    @GetMapping("/hello")
+    public String sayHello(Model theModel){
+
+        theModel.addAttribute("theDate",java.time.LocalDateTime.now());
+
+
         return "helloworld";
 
 
